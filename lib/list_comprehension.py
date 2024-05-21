850d79e8c5e5a7f9f4385cb4acefb28c48f7d12f
Sumnicky99 <sumnicky99@gmail.com>
2024-05-21 14:55:47 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # Using list comprehension to filter out even numbers from num_list
+    even_numbers = [num for num in num_list if num % 2 == 0]
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    if not sentence_list:  # Check if the sentence_list is empty
+        return []  # Return an empty list if it is empty
+    else:
+        # Using list comprehension to add exclamation mark to each sentence
+        exclamated_sentences = [sentence + '!' for sentence in sentence_list]
+        return exclamated_sentences
